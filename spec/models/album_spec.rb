bd3caa75df49fe6ec0450756eef430d550967acf
Rian Carter <R.Carter86@hotmail.com>
2022-05-25 20:39:19 -0700

    more practice

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -6,3 +6,10 @@ describe Album do
   }
   it { should validate_length_of(:name).is_at_most(100)}
 end
+
+describe Album do
+  it("titleizes the name of an album") do
+    album = Album.create({name: "giant steps", genre: "jazz"})
+    expect(album.name()).to(eq("Giant Steps"))
+  end
+end
